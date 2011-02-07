16343bf5b0299292b0429b23e66374324524c230
James Sullivan <jsullivan@cambridgecloudpartners.com>
2011-02-07 15:48:45 -0500

    added caching so salesforce is hit less often and decreases avg response time, added validation output on template, moved settings to a flask config file

--- mysite.py
+++ mysite.py
@@ -1,49 +1,52 @@
-
-
 from flask import Flask, request, flash, redirect, url_for, render_template
 from flaskext.wtf import Form, TextField, HiddenField, Required
+from flaskext.cache import Cache
 from sforce.partner import SforcePartnerClient
-from settings import *
+
+sfdc_connection = 1
 
 app = Flask(__name__)
-app.secret_key = 'make something long and complicated here - orange!'
-
-h = SforcePartnerClient('partner.wsdl')
-h.login(SALESFORCE_USERNAME, SALESFORCE_PASSWORD, SALESFORCE_TOKEN)
-
+app.config.from_pyfile('mysite.cfg')
+cache = Cache(app)
 
 class SignupForm(Form):
     firstname = TextField("First Name", validators=[Required()])
     lastname = TextField("Last Name", validators=[Required()])
     company = TextField("Company", validators=[Required()])
 
-
-@app.route('/', methods=['GET','POST'])
-def hello_world():
-    
-    form = SignupForm()
-    if form.validate_on_submit():
-        lead = h.generateObject('Lead')
-        lead.FirstName = form.firstname.data
-        lead.LastName = form.lastname.data
-        lead.Company = form.company.data
-        result = h.create(lead)
-        if result.success:
-            return redirect(url_for("thank_you"))
-    result = h.query('SELECT Company FROM Lead limit 10')
+@cache.cached(timeout=60*10, key_prefix='get_salesforce_lead_companies')
+def get_salesforce_lead_companies():
+    result = sfdc().query('SELECT Company FROM Lead limit 10')
     companies = []
     for record in result.records:
         companies.append( record.Company )
+    return companies
     
+@app.route('/', methods=['GET','POST'])
+def hello_world():
+    form = SignupForm()
+    if form.validate_on_submit():
+        lead = sfdc().generateObject('Lead')
+        lead.FirstName = form.firstname.data
+        lead.LastName = form.lastname.data
+        lead.Company = form.company.data
+        try:
+            result = sfdc().create(lead)
+        except Exception as e:
+            flash(result)
+        if result.success:
+            return redirect(url_for("thank_you"))
+    companies = get_salesforce_lead_companies()
     return render_template("index.html",companies=companies, form=form)
 
 @app.route('/thank_you', methods=['GET'])
 def thank_you():
     return render_template("thank_you.html")
     
-    
-    
+def sfdc():
+    h = SforcePartnerClient('partner.wsdl')
+    h.login(app.config['SALESFORCE_USERNAME'], app.config['SALESFORCE_PASSWORD'], app.config['SALESFORCE_TOKEN'])
+    return h
     
 if __name__ == '__main__':
-    app.debug = True
     app.run()
